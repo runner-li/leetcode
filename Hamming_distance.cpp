85c034fd685dcbab231bbb9007d45a5ee8056009
runner-li <yueqian.li@gmail.com>
2017-08-04 20:34:30 -0700

    Create Hamming_distance.cpp

---
+++ Hamming_distance.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) 
+    {
+        int distance = 0;
+        int z;
+        z =x^y;
+        while (0!=z)
+        {
+            distance += z & 1;
+            z = z>>1;
+        }
+        return distance;
+    }
+};
